6cadc5980c2b95bd68d69bb10eeb3818c33a6c63
Cihat Akviranli <cihatakv@gmail.com>
2019-12-03 23:23:26 -0600

    day20.selfTest added

---
+++ src/day20/selfTest/W3SchoolsExample01.java
@@ -0,0 +1,13 @@
+package day20.selfTest;
+
+public class W3SchoolsExample01 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++){
+            if (i %2 == 0)
+                System.out.println(i);
+        }
+
+    }
+
+}
