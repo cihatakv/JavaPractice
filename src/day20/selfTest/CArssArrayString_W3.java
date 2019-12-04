c908eeef12a37c6b126ff6557da4ec5c13ccfc9a
Cihat Akviranli <cihatakv@gmail.com>
2019-12-03 23:34:44 -0600

    day20.selfTest added

---
+++ src/day20/selfTest/CArssArrayString_W3.java
@@ -0,0 +1,11 @@
+package day20.selfTest;
+
+public class CArssArrayString_W3 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW" , "Ford" , "Mazda"};
+        for (String i : cars){
+            System.out.println(i);
+        }
+
+    }
+}
